a3be729f3bdee0572adb002bcc77bfe553c4a8db
Johan Cornelissen <johancornelissen@Johans-MacBook-Pro.local>
2017-11-01 14:40:43 -0400

    Updated expected output files for CREATEACCT tests, and included tests for no transaction file in the test verification script.

--- testOutputVerificationScript.sh
+++ testOutputVerificationScript.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# STDOUT Colour Definitions (NC stands for No Colour)
 RED='\033[0;31m';
 BLUE='\033[0;34m';
 GREEN='\033[0;32m';
 NC='\033[0m';
+
+passed=0;
+failed=0;
 
 while read p; do
   #echo $p
@@ -27,10 +31,25 @@ while read p; do
   ExpectedOutputFile="Testing/${pathArray[0]}/Expected Output Files/output_${pathArray[1]}.txt"
   
   echo -e "${BLUE}Verifying ${pathArray[0]}/${pathArray[1]} test:${NC}";
-  echo "diff \"${ActualTransactionFile}\" \"${ExpectedTransactionFile}\"";
-  OUTPUT1="$(diff ${ActualTransactionFile} ${ExpectedTransactionFile})"''
-  echo "${OUTPUT1}";
   
+  # If no transaction file should be created, verify no transaction got created.
+  # These situations use special keyword NO_TRANSACTION_FILE
+  if [ ${pathArray[2]} = "NO_TRANSACTION_FILE" ]; then
+	echo "Verifying no transaction file was created by this test."
+	if [ ! -f ${ActualTransactionFile} ]; then
+	    echo -e "Verified no transaction file was created.\n";
+	else
+		OUTPUT1="Error, a transaction file WAS created by the test. See file, ${ActualTransactionFile}";
+		echo -e "${OUTPUT1}\n";
+	fi  
+  else
+	# Test if transaction file matches the expected one.
+  	echo "diff \"${ActualTransactionFile}\" \"${ExpectedTransactionFile}\"";
+  	OUTPUT1="$(diff ${ActualTransactionFile} ${ExpectedTransactionFile})"''
+  	echo "${OUTPUT1}";
+  fi
+  
+  # Test if the stdout file matches the expected one.
   echo "diff \"${ActualOutputFile}\" \"${ExpectedOutputFile}\"";
   OUTPUT2="$(diff ${ActualOutputFile} ${ExpectedOutputFile})"''
   echo "${OUTPUT2}";
@@ -38,11 +57,20 @@ while read p; do
   # PRINT PASS OR FAIL
   if [ -z "${OUTPUT1}" ] && [ -z "${OUTPUT2}" ]; then
 	  echo -e "${GREEN}${pathArray[0]}/${pathArray[1]} TEST PASSED!${NC}";
+	  passed=$((passed+1));
   else
 	  echo -e "${RED}${pathArray[0]}/${pathArray[1]} TEST FAILED!${NC}";
+	  failed=$((failed+1));
   fi
   
   # New line
   echo "";
   
 done <testsToRun.txt
+
+echo "#####################TESTING STATISTICS#####################";
+echo -e "${GREEN}Tests Passed: $passed ${NC}";
+echo -e "${RED}Tests Failed: $failed ${NC}";
+testsRun=$((passed+failed));
+echo -e "${BLUE}Tests Run : $testsRun ${NC}";
+echo "############################################################";
