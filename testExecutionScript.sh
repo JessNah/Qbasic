2983c42e084ec2f7c51b55aa898dc279248864a9
Johan Cornelissen <johancornelissen@Johans-MacBook-Pro.local>
2017-11-03 13:03:07 -0400

    Added comments to testing scripts, and added Failure Report Document

--- testExecutionScript.sh
+++ testExecutionScript.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
+# STDOUT Colour Definitions (NC stands for No Colour)
 RED='\033[0;31m';
 NC='\033[0m';
 
+# Iterate over all the lines in the testsToRun.txt file.
 while read p; do
-  #echo $p
-  IFS=',' ;
-  index=0;
-  
-  # Skip blank lines
+
+  # Skip blank lines (used purely for readability of testsToRun.txt file)
   if [ -z "$p" ]; then
 	  continue
   fi
   
+  # Create an array of the comma seperated values found for each test case.
+  # Where 
+  # 	pathArray[0] = test area name, ex: LOGIN, LOGOUT, etc.
+  #		pathArray[1] = test case name
+  #		pathArray[2] = transaction summary file name
+  IFS=',' ;
+  index=0;
   for path in $p; do 
   	pathArray[index]=$path;
 	#echo $path; 
 	index=$index+1;
   done
   
+  # Create the path to valid accounts file, transaction summary file, standard input file, and the standard output file path
   ValidAccountsFile="Testing/${pathArray[0]}/Input Files/valid_accounts_file_${pathArray[1]}.txt"
   TransactionFile="Testing/${pathArray[0]}/Output Files/${pathArray[2]}.txt"
   InputFile="Testing/${pathArray[0]}/Input Files/input_${pathArray[1]}.txt"
   OutputFile="Testing/${pathArray[0]}/Output Files/output_${pathArray[1]}.txt"
   
-  
+  # Display in red text what test is currently being verified.
   echo -e "${RED}Executing ${pathArray[0]}/${pathArray[1]} test:${NC}";
+  
+  # Display what execution line is being executed in the rare case where manual intervention is necessary.
   echo "./QBasic.py \"${ValidAccountsFile}\" \"${TransactionFile}\" < \"${InputFile}\" > \"${OutputFile}\"";
   ./QBasic.py ${ValidAccountsFile} ${TransactionFile} < ${InputFile} > ${OutputFile};
+  
+  # Display where test output can be found for ease of debugging test failures.
   echo "${pathArray[0]}/${pathArray[1]} test output saved to ${OutputFile}";
   
-  # New line
+  # Print new line to seperate the test case output and results
   echo "";
   
 done <testsToRun.txt
--- testOutputVerificationScript.sh
+++ testOutputVerificationScript.sh
@@ -6,34 +6,42 @@ BLUE='\033[0;34m';
 GREEN='\033[0;32m';
 NC='\033[0m';
 
+# Statistic counters for number of passed/failed test cases
 passed=0;
 failed=0;
 
+# Iterate over all the lines in the testsToRun.txt file.
 while read p; do
-  #echo $p
-  IFS=',' ;
-  index=0;
   
-  # Skip blank lines
+  # Skip blank lines (used purely for readability of testsToRun.txt file)
   if [ -z "$p" ]; then
 	  continue
   fi
   
+  # Create an array of the comma seperated values found for each test case.
+  # Where 
+  # 	pathArray[0] = test area name, ex: LOGIN, LOGOUT, etc.
+  #		pathArray[1] = test case name
+  #		pathArray[2] = transaction summary file name
+  IFS=',' ;
+  index=0;
   for path in $p; do 
   	pathArray[index]=$path;
 	#echo $path; 
 	index=$index+1;
   done
   
+  # Create the path to actual/expected versions of the transaction summary file and standard output files.
   ActualTransactionFile="Testing/${pathArray[0]}/Output Files/${pathArray[2]}.txt"
   ExpectedTransactionFile="Testing/${pathArray[0]}/Expected Output Files/${pathArray[2]}.txt"
   ActualOutputFile="Testing/${pathArray[0]}/Output Files/output_${pathArray[1]}.txt"
   ExpectedOutputFile="Testing/${pathArray[0]}/Expected Output Files/output_${pathArray[1]}.txt"
   
+  # Display in blue text what test is currently being verified.
   echo -e "${BLUE}Verifying ${pathArray[0]}/${pathArray[1]} test:${NC}";
   
   # If no transaction file should be created, verify no transaction got created.
-  # These situations use special keyword NO_TRANSACTION_FILE
+  # These situations use special keyword NO_TRANSACTION_FILE.
   if [ ${pathArray[2]} = "NO_TRANSACTION_FILE" ]; then
 	echo "Verifying no transaction file was created by this test."
 	if [ ! -f ${ActualTransactionFile} ]; then
@@ -43,7 +51,7 @@ while read p; do
 		echo -e "${OUTPUT1}\n";
 	fi  
   else
-	# Test if transaction file matches the expected one.
+	# Otherwise if a transaction file IS expected, test if transaction file matches the expected one.
   	echo "diff \"${ActualTransactionFile}\" \"${ExpectedTransactionFile}\"";
   	OUTPUT1="$(diff ${ActualTransactionFile} ${ExpectedTransactionFile})"''
   	echo "${OUTPUT1}";
@@ -54,7 +62,7 @@ while read p; do
   OUTPUT2="$(diff ${ActualOutputFile} ${ExpectedOutputFile})"''
   echo "${OUTPUT2}";
   
-  # PRINT PASS OR FAIL
+  # PRINT PASS OR FAIL for the test cases (using green or red text appropriately)
   if [ -z "${OUTPUT1}" ] && [ -z "${OUTPUT2}" ]; then
 	  echo -e "${GREEN}${pathArray[0]}/${pathArray[1]} TEST PASSED!${NC}";
 	  passed=$((passed+1));
@@ -63,11 +71,12 @@ while read p; do
 	  failed=$((failed+1));
   fi
   
-  # New line
+  # Print new line to seperate the test case output and results
   echo "";
   
 done <testsToRun.txt
 
+# Print to the user at the end of the verification phase the statistics of tests passed versus failed.
 echo "#####################TESTING STATISTICS#####################";
 echo -e "${GREEN}Tests Passed: $passed ${NC}";
 echo -e "${RED}Tests Failed: $failed ${NC}";
